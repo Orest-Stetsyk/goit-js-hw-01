18992cbae8d6834e7e141481ad5ece8d3a8a864e
Orest-Stetsyk <62755155+Orest-Stetsyk@users.noreply.github.com>
2024-01-11 21:38:53 +0200

    2 i 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,3 @@
 function getShippingMessage(country, price, deliveryFee) {
-    return "Shipping to <country> will cost <totalPrice> credits";
+    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
